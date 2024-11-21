4c317d3a10efd251c1f7aabed66f470a525b6176
FernandoKatano <fernandokatano@yahoo.com.br>
2024-11-21 05:54:45 -0300

    Página de welcome definida e easteregg adicionado

--- app/src/main/java/br/unimes/portal/vanessa/Welcome.kt
+++ app/src/main/java/br/unimes/portal/vanessa/Welcome.kt
@@ -1,6 +1,7 @@
 package br.unimes.portal.vanessa
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.unimes.portal.vanessa.databinding.ActivityWelcomeBinding
@@ -8,19 +9,30 @@ import br.unimes.portal.vanessa.databinding.ActivityWelcomeBinding
 
 class Welcome : AppCompatActivity() {
     private lateinit var binding: ActivityWelcomeBinding
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnWelcomecad.setOnClickListener {
+        mediaPlayer = MediaPlayer.create(this, R.raw.yoshi)
+
+        binding.imgFeflogoWelcome.setOnClickListener{
+            mediaPlayer.start()
+        }
+        binding.btnRegisterWelcome.setOnClickListener {
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
-        binding.btnWelcomelogin.setOnClickListener {
+        binding.btnLoginWelcome.setOnClickListener {
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
+
+    }
+    override fun onDestroy() {
+        mediaPlayer.release()
+        super.onDestroy()
     }
 }
