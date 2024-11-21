6cb8198266818cc6573ad53881bf1426b26000ee
FernandoKatano <fernandokatano@yahoo.com.br>
2024-11-21 02:50:09 -0300

    Criação da Splash Screen Pássaro.png adicionado a res Logotipo_vanessa.png adicionado a res Logotipo_F&F.png adicionado a res

--- app/src/main/java/br/unimes/portal/vanessa/Loading.kt
+++ app/src/main/java/br/unimes/portal/vanessa/Loading.kt
@@ -1,20 +1,22 @@
 package br.unimes.portal.vanessa
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Loading : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_loading)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.imv_df)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Welcome::class.java)
+            val options = ActivityOptions.makeCustomAnimation(this, android.R.anim.fade_in, android.R.anim.fade_out)
+            startActivity(intent, options.toBundle())
+            finish()
+        }, 3000)
         }
     }
-}
